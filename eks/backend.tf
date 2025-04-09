30752b29f8484db44af4426d52cb677e66ff9e12
Mohamed Akram LARAFA <larafa.akram@gmail.com>
2025-04-09 17:16:18 +0100

    Update: chengement de repo

---
+++ eks/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.11.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "praduman-eks-s3"
+    region         = "ap-south-1"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region  = var.aws-region
+}
